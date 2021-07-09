de6028f7b627d899f8a2027af138f1c729ceba13
Wataru-Oshima-Tokyo <wataruoshima@my.smccd.edu>
2021-07-09 17:01:40 +0900

    first commit

---
+++ include/go_back_recovery/go_back_recovery.h
@@ -0,0 +1,38 @@
+#ifndef GO_BACK_RECOVERY_H_
+#define GO_BACK_RECOVERY_H_
+#include <nav_core/recovery_behavior.h>
+#include <costmap_2d/costmap_2d_ros.h>
+#include <tf/transform_listener.h>
+#include <ros/ros.h>
+#include <base_local_planner/costmap_model.h>
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Point.h>
+#include <angles/angles.h>
+#include <sensor_msgs/LaserScan.h>
+
+#define RANGE_MAX 5.6
+
+namespace go_back_recovery{
+ class GoBackRecovery : public nav_core::RecoveryBehavior {
+  protected :
+   GoBackRecovery();
+   void initialize(std::string name, tf::TransformListener* tf, costmap_2d::Costmap2DROS* global_costmap, costmap_2d::Costmap2DROS* local_costmap);
+   void runBehavior();
+   ~GoBackRecovery();
+  private :
+    costmap_2d::Costmap2DROS* global_costmap_, *local_costmap_;
+    costmap_2d::Costmap2D costmap_;
+    std::string name_;
+    tf::TransformListener* tf_;
+    ros::NodeHandle n;
+    ros::Publisher vel_pub;
+    ros::Subscriber scan_sub;
+    geometry_msgs::Twist cmd_vel;
+    bool initialized_;
+    base_local_planner::CostmapModel* world_model_;
+    double null_check(double target);
+    void scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg);
+    int sub_n, sub_flag;
+  };
+};
+#endif
